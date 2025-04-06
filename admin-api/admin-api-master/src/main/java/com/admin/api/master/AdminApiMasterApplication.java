2905ddf943702bdeb6635cc2bd77c76fd975eb6b
Jinwook Kim <jinwook.kim@riman.com>
2025-04-06 13:48:33 -0400

    init

---
+++ admin-api/admin-api-master/src/main/java/com/admin/api/master/AdminApiMasterApplication.java
@@ -0,0 +1,13 @@
+package com.admin.api.master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminApiMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApiMasterApplication.class, args);
+    }
+
+}
